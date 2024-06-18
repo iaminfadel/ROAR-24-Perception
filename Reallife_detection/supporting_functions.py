d94b6f3f0b513c5f9a3a7479b99de59286653796
OmarEmadAldin <omaremadeldinhamed@gmail.com>
2024-06-18 16:50:16 +0300

    Reallife_detection

---
+++ Reallife_detection/supporting_functions.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+import numpy as np
+import scipy
+import scipy.optimize
+
+def tlbr_to_center1(boxes):
+    points = []
+    for tlx, tly, brx, bry in boxes:
+        cx = (tlx+brx)/2
+        cy = (tly+bry)/2
+        points.append([cx, cy])
+    return points
+
+def tlbr_to_corner(boxes):
+    points = []
+    for tlx, tly, brx, bry in boxes:
+        cx = (tlx+tlx)/2
+        cy = (tly+tly)/2
+        points.append((cx, cy))
+    return points
+
+def tlbr_to_corner_br(boxes):
+    points = []
+    for tlx, tly, brx, bry in boxes:
+        cx = (brx+brx)/2
+        cy = (bry+bry)/2
+        points.append((cx, cy))
+    return points
+
+def tlbr_to_area(boxes):
+    areas = []
+    for tlx, tly, brx, bry in boxes:
+        cx = (brx-tlx)
+        cy = (bry-tly)
+        areas.append(abs(cx*cy))
+    return areas
+
+def get_horiz_dist_centre(boxes):
+    pnts1 = np.array(tlbr_to_center1(boxes[0]))[:,0]
+    pnts2 = np.array(tlbr_to_center1(boxes[1]))[:,0]
+    return pnts1[:,None] - pnts2[None]
+
+def get_horiz_dist_corner_tl(boxes):
+    pnts1 = np.array(tlbr_to_corner(boxes[0]))[:,0]
+    pnts2 = np.array(tlbr_to_corner(boxes[1]))[:,0]
+    return pnts1[:,None] - pnts2[None]
+
+def get_horiz_dist_corner_br(boxes):
+    pnts1 = np.array(tlbr_to_corner_br(boxes[0]))[:,0]
+    pnts2 = np.array(tlbr_to_corner_br(boxes[1]))[:,0]
+    return pnts1[:,None] - pnts2[None]
+
+def get_vertic_dist_centre(boxes):
+    pnts1 = np.array(tlbr_to_center1(boxes[0]))[:,1]
+    pnts2 = np.array(tlbr_to_center1(boxes[1]))[:,1]
+    return pnts1[:,None] - pnts2[None]
+
+def get_area_diffs(boxes):
+    pnts1 = np.array(tlbr_to_area(boxes[0]))
+    pnts2 = np.array(tlbr_to_area(boxes[1]))
+    return abs(pnts1[:,None] - pnts2[None])
+
+centre = 320
+
+def get_dist_to_centre_tl(box, cntr = centre):
+    pnts = np.array(tlbr_to_corner(box))[:,0]
+    return abs(pnts - cntr)
+
+
+def get_dist_to_centre_br(box, cntr = centre):
+    pnts = np.array(tlbr_to_corner_br(box))[:,0]
+    return abs(pnts - cntr)
+
+def get_cost(boxes, lbls = None, sz1 = 400):
+    alpha = sz1; beta  = 10; gamma = 5
+    
+    #vertical_dist, scale by gamma since can't move up or down
+    vert_dist = gamma*abs(get_vertic_dist_centre(boxes))
+    
+    #horizonatl distance.
+    horiz_dist = get_horiz_dist_centre(boxes)
+    
+    #increase cost if object has moved from right to left.
+    horiz_dist[horiz_dist<0] = beta*abs(horiz_dist[horiz_dist<0])
+    
+    #area of box
+    area_diffs = get_area_diffs(boxes)/alpha
+    
+    cost = np.array([vert_dist,horiz_dist,area_diffs])
+    
+    cost=cost.sum(axis=0)
+    
+    #add penalty term for different object classes
+    if lbls is not None:
+        for i in range(cost.shape[0]):
+            for j in range(cost.shape[1]):
+                if (lbls[0][i]!=lbls[1][j]):
+                    cost[i,j]+=150
+    return cost
+   
+def get_tracks(cost):
+    return scipy.optimize.linear_sum_assignment(cost)
+    
+
+def get_tracks_ij(cost):
+    tracks = scipy.optimize.linear_sum_assignment(cost)
+    return [[i,j] for i, j in zip(*tracks)]
